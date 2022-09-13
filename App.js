2b89012d8134968b6c71daf85f15be58af887370
repsukka <81526112+repsukka@users.noreply.github.com>
2022-09-13 17:27:21 +0300

    flatlist added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { Image, ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View, StatusBar, FlatList} from 'react-native';
 import {DATA} from './Data';
 
 
@@ -7,18 +7,36 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
-      <Text>My app</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <Text>{item.fname}</Text>}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+
+const Item = ({person}) => {
+  return(
+    <View>
+      
+    </View>
+  )
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#fff',
-    marginTop: StatusBar.currentHeight || 0
+    marginTop: (StatusBar.currentHeight || 0)+20
+  },
+  text:{
+    padding: 10,
+    fontSize: 40
   }
 });
